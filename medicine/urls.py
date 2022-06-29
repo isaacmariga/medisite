d5b86d6af4d4e135ba69b365500e6affbfeda366
isaac mariga <isaacwangombe5@gmail.com>
2022-06-29 15:45:43 +0300

    adding heroku to link

--- medicine/urls.py
+++ medicine/urls.py
@@ -5,7 +5,7 @@ from . import views, api_view
 
 urlpatterns = [
   path('', views.welcome,name = 'test'),
-  path('api/profile/', api_view.ProfileList.as_view()),
+  path('api/profile/', api_view.ProfileList.as_view(), name = 'profile'),
   path('api/supplier/', api_view.SupplierList.as_view()),
   path('api/medicine/', api_view.MedicineList.as_view()),
   path('api/donation/', api_view.DonatingList.as_view()),
