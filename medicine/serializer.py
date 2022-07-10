8834d19d003ab1e901d9606dae63c03b69014b99
isaac mariga <isaacwangombe5@gmail.com>
2022-07-10 07:06:13 +0300

    changing field names

--- medicine/serializer.py
+++ medicine/serializer.py
@@ -56,4 +56,4 @@ class MediUnitsSerializer(serializers.ModelSerializer):
 class CalculationUnitsSerializer(serializers.ModelSerializer):
     class Meta:
       model = CalculationUnits
-      fields = ('sales','units', 'price', 'donations')
+      fields = ('units_sold','units', 'set_price', 'donation_amount')
